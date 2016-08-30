584c59761cca737b147463ee7e345a0a5ce9b242
lgl <468577977@qq.com>
2016-08-30 10:43:34 +0800

    我是一个本地的Master

--- app/src/main/java/com/lgl/myimageload/MainActivity.java
+++ app/src/main/java/com/lgl/myimageload/MainActivity.java
@@ -8,11 +8,5 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//我是开发者的改变
-        //我是开发者添加的代码
-    }
-
-    //我是开发者分支在修改的东西
-    public void saaa(){
-
     }
 }
