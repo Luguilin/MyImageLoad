7631e1ab67194d7a8814836107f5f5a5094e478e
lgl <468577977@qq.com>
2016-08-30 09:59:50 +0800

    我是Develop

--- app/src/main/java/com/lgl/myimageload/MainActivity.java
+++ app/src/main/java/com/lgl/myimageload/MainActivity.java
@@ -8,5 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//我是开发者的改变
+        //我是开发者添加的代码
     }
 }
